a84a907ace146b1e66d5e49cffc5276063c26771
Masahjor <madssahl@outlook.dk>
2025-05-01 13:38:59 +0200

    Header work - start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- Script -->
+<script lang="ts">
+    import '../app.css';
+    import Header from './Header.svelte';
+    let { children } = $props();
+
+</script>
+
+
+<!-- HTML -->
+<main>
+    <Header />
+
+    
+    <div class="test-container">
+        <h1>Welcome to SvelteKit</h1>
+        <p>To get started, edit <code>src/routes/+page.svelte</code> and save to reload.</p>
+        <p><a href="https://kit.svelte.dev/docs/introduction">Documentation</a></p>
+        <p><a href="https://svelte.dev/repl">REPL</a></p>
+    </div>
+</main>
+
+
+<!-- CSS -->
+<style>
+    .test-container {
+        background-color: #333;
+        padding: 1em;
+        max-width: 600px;
+        margin: auto;
+    }
+    h1 {
+        color: #ff3e00;
+        font-size: xx-large;
+        padding-bottom: 15px;
+    }
+    p {
+        color: #f4f4f4;
+    }
+    a {
+        color: #ff3e00;
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    code {
+        background: #ff3e00;
+        padding: 0.2em 0.4em;
+        border-radius: 3px;
+    }
+    @media (prefers-color-scheme: dark) {
+        .test-container {
+            background: #333;
+            color: #fff;
+        }
+
+    }
+</style>
+
+
+<!-- End of code snippet -->
---
+++ src/routes/Header.svelte
@@ -0,0 +1,65 @@
+<!-- script -->
+<script lang="ts">
+    
+</script>
+
+<!-- HTML -->
+<main>
+    <header>
+        <figure>
+            <!-- svelte-ignore a11y_img_redundant_alt -->
+            <img src="evilmads.jpg"alt="My photo" width="20" height="20" class="profile-pic">
+        </figure>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a class="active" href="/about">About me</a></li>
+            <li><a href="/education">Education</a></li>
+            <li><a href="/art">Art Gallery</a></li>
+            <li><a href="/it">IT Work</a></li>
+        </ul>
+    </nav>
+</main>
+
+<!-- CSS -->
+<style>
+    main {
+        background-color: hotpink;
+        padding: 1em;
+        height: auto;
+        margin: auto;
+    }
+
+    nav {
+        position: relative;
+        background-color: #333;
+        overflow: hidden;
+    }
+
+    nav ul {
+        display: flex;
+        justify-content: center; /* Center the list items */
+        padding: 0;
+    }
+
+    nav a {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    nav a:hover {
+        background-color: yellow;
+        color: black;
+    }
+
+    nav a.active {
+        background-color: blue;
+        color: white;
+    }
+    
+</style>
